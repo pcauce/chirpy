1e361d204c90a473d63def9c30345e36f4c59303
pcauce <pcaucev@gmail.com>
2025-05-30 02:11:10 +0200

    (3) ch1_l11

--- main.go
+++ main.go
@@ -9,12 +9,20 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/logo.png", http.FileServer(http.Dir("./assets")))
+
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
